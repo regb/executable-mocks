8a77324df2dd8f679ee7ead42527d1572a817c04
Maria Prat <mariaprat@google.com>
2020-07-23 16:28:55 +0200

    Address Régis's review

--- example-codes/fakessim2qff.go
+++ example-codes/fakessim2qff.go
@@ -5,28 +5,35 @@ package main
 
 import (
         "os"
-	"log"
 	"io/ioutil"
 )
+
+func verifyInput() {
+	fIn, err := os.Open(os.Args[6])
+	if err != nil  {
+		os.Exit(1) 
+	}
+	content, err := ioutil.ReadFile(fIn.Name())
+	if err != nil {
+		os.Exit(1) 
+	}
+	if string(content) != "I'm an input file.\n" {  // TODO: Check if the content is right
+		os.Exit(1)
+	}
+}
+
+func generateOutput() {
+	err := ioutil.WriteFile(os.Args[4], []byte("I'm an output file.\n"), 0666) // TODO: Write right content
+        if err != nil {
+		os.Exit(1)
+	}
+}
 
 func main() {
 	if len(os.Args) != 7 || os.Args[1] != "-t" || os.Args[2] != "201809010005" || os.Args[3] != "-o" || os.Args[5] != "-d"  {
                 os.Exit(1)
         }
-        fIn, err := os.Open(os.Args[6])
-	if err != nil  {
-		log.Fatal(err) 
-	}
-	content, err := ioutil.ReadFile(fIn.Name())
-	if err != nil {
-		log.Fatal(err)
-	}
-	if string(content) != "I'm an input file.\n" {  // TODO: Check if the content is right
-		os.Exit(1)
-	}
-	err = ioutil.WriteFile(os.Args[4], []byte("I'm an output file.\n"), 0666) // TODO: Write right content
-        if err != nil {
-		log.Fatal(err)
-	}
+	verifyInput()
+	generateOutput()
 	os.Exit(0)
 }
--- example-codes/fakessim2qff.sh
+++ example-codes/fakessim2qff.sh
@@ -1,14 +1,32 @@
 #!/bin/sh
-# correct usage example
+# example 1: correct usage
 echo "I'm an input file." > input
 ./fakessim2qff -t '201809010005' -o output -d input
-if [ $? -eq 1 ]
+if [ $? -ne 0 ] || [ ! -f "./output" ] || [ "$(cat output)" != "I'm an output file." ]
 then
 	echo "Test 1: not working properly."
 fi
-# incorrect usage example
-./fakessim2qff -t '20180901005' -d input
+
+# example 2: incorrect usage (misses output flags)
+echo "I'm an input file." > input
+./fakessim2qff -t '201809010005' -d input
 if [ $? -eq 0 ]
 then
 	echo "Test 2: not working properly."
+fi
+
+# example 3: incorrect usage (wrong input file content)
+echo "I'm an incorrect input file." > input
+./fakessim2qff -t '201809010005' -o output -d input
+if [ $? -eq 0 ]
+then
+	echo "Test 3: not working properly."
+fi
+
+# example 4: incorrect usage (wrong timestamp)
+echo "I'm an input file." > input
+./fakessim2qff -t '0' -o output -d input
+if [ $? -eq 0 ]
+then
+	echo "Test 4: not working properly."
 fi
