d54b7b1a406ae3583368f522c005f289eedf1730
Sabina Dayanova <sdayanova@google.com>
2020-08-20 20:20:55 +0500

    Add hash check and writing the output

--- cmd/mockexec/main.go
+++ cmd/mockexec/main.go
@@ -4,12 +4,19 @@ import (
   "io/ioutil"
   "log"
   "os"
-  
+  "io"
+  "bytes"
+  "encoding/hex"
+  "crypto/sha256"
+
   "github.com/golang/protobuf/proto"
   pb "github.com/regb/executable-mocks/protos/mockexec"
 )
 
+const bufferSize = 4096
+
 func main() {
+  // TODO: obtain the configuration file from the arguments
   t, err := ioutil.ReadFile("configs/flights.textproto")
   if err != nil {
     log.Fatal(err)
@@ -21,7 +28,7 @@ func main() {
   if len(os.Args) - 1 != len(c.Args) {
     log.Fatalf("Expected %v arguments but got %v", len(c.Args), len(os.Args) - 1)
   }
-  
+
   var sourcePath string
   var destPath string
   for i := 0; i < len(os.Args); i++ {
@@ -35,8 +42,26 @@ func main() {
         log.Fatalf("String arguments don't match: want '%s'; got '%s'", x.StrArg, os.Args[i])
       }
     case *pb.Argument_InHash:
-      // TODO: check the hash.
-      log.Printf("Would check input file: %v", os.Args[i])
+      fIn, err := os.Open(os.Args[i])
+      if err != nil {
+        log.Fatal(err)
+      }
+
+      hash := sha256.New()
+      if _, err = io.CopyBuffer(hash, fIn, make([]byte, bufferSize)); err != nil {
+      	log.Fatal(err)
+      }
+
+      expectedHash, err := hex.DecodeString(x.InHash)
+      if err != nil {
+        log.Fatal(err)
+      }
+
+      if !bytes.Equal(hash.Sum(nil), expectedHash) {
+        log.Fatalf("The hash of the file %v doesn't match the expected one", os.Args[i])
+      }
+
+      fIn.Close()
     case *pb.Argument_OutPath:
       sourcePath = x.OutPath
       destPath = os.Args[i]
@@ -44,7 +69,23 @@ func main() {
       log.Fatalf("Unexpected argument type %T", x)
     }
   }
-  // TODO: if we made it this far and it's necessary, write the desired content
-  //       to the output file.
-  log.Printf("Would produce '%s' to: %v", sourcePath, destPath)
+  if sourcePath != "" && destPath != "" {
+    fDest, err := os.Create(destPath)
+    if err != nil {
+       log.Fatal(err)
+    }
+
+    fSrc, err := os.Open(sourcePath)
+    if err != nil {
+       log.Fatal(err)
+    }
+
+    if _, err = io.CopyBuffer(fDest, fSrc, make([]byte, bufferSize)); err != nil {
+      log.Fatal(err)
+    }
+  } else if sourcePath != "" {
+    log.Fatalf("Destination path is empty while source path is not")
+  } else if destPath != "" {
+    log.Fatalf("Source path is empty while destination path is not")
+  }
 }
