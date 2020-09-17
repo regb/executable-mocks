b4d859da9747b8f6534e0c0c8a3048b4d1c1a8cf
Maria Prat <mariaprat@google.com>
2020-09-17 20:06:22 +0200

    Address Régis comment

--- cmd/bldmock/bldmock.go
+++ cmd/bldmock/bldmock.go
@@ -2,17 +2,39 @@ package bldmock
 
 import (
   "log"
+  "crypto/sha256"
+  "os"
+  "io"
+  "fmt"
   
   pb "github.com/regb/executable-mocks/protos/mockexec"
 )
 
 type StrArg string
-type InHash string
+type InFile string
 type OutPath string
 type OutContent string
 
+
+const bufferSize = 4096
+
+// Takes the path to a file and outputs the SHA256 checksum of the file
+// in an hexadecimal format.
+func generateFileHash(file string) string {
+	fIn, err := os.Open(file)
+  if err != nil {
+  	log.Fatal(err)
+	}
+	defer fIn.Close()
+	hash := sha256.New()
+	if _, err = io.CopyBuffer(hash, fIn, make([]byte, bufferSize)); err != nil {
+		log.Fatal(err)
+	}
+	return fmt.Sprintf("%x", hash.Sum(nil))
+}
+
 // Takes the name of the binary and a sequence of arguments and returns a MockCall.
-// Each argument is a string of one of the following four types: StrArg, InHash, OutPath, OutContent
+// Each argument is a string of one of the following four types: StrArg, InFile, OutPath, OutContent
 func BuildMockConfig(name string, args ...interface{}) pb.MockCall {
   
   mc := pb.MockCall {
@@ -24,8 +46,8 @@ func BuildMockConfig(name string, args ...interface{}) pb.MockCall {
     switch t := arg.(type) {
       case StrArg:
         mc.Args = append(mc.Args, &pb.Argument{Arg: &pb.Argument_StrArg{string(arg.(StrArg))}})
-      case InHash:
-        mc.Args = append(mc.Args, &pb.Argument{Arg: &pb.Argument_InHash{string(arg.(InHash))}})
+      case InFile:
+        mc.Args = append(mc.Args, &pb.Argument{Arg: &pb.Argument_InHash{generateFileHash(string(arg.(InFile)))}})
       case OutPath:
         mc.Args = append(mc.Args, &pb.Argument{Arg: &pb.Argument_OutPath{string(arg.(OutPath))}})
       case OutContent:
--- test/bldmocktest.go
+++ test/bldmocktest.go
@@ -2,14 +2,23 @@ package main
 
 import (
   "log"
-
+  "io/ioutil"
+  
   "github.com/regb/executable-mocks/cmd/bldmock"
   "github.com/google/go-cmp/cmp"
   "google.golang.org/protobuf/testing/protocmp"
   pb "github.com/regb/executable-mocks/protos/mockexec"
 )
 
+func createInFiles(filePath string, content string) string {
+  if err := ioutil.WriteFile(filePath, []byte(content), 0700); err != nil {
+      log.Fatal(err)
+  }
+  return filePath
+}
+
 func Test() {
+  fIn := createInFiles("tmp/test-input", "INPUT\n")
   for i, tt := range []struct {
     binary string
     args []interface{}
@@ -18,7 +27,7 @@ func Test() {
     {
     binary: "example",
     args: []interface{} { bldmock.StrArg("-t"), bldmock.StrArg("SOME STRING"), bldmock.StrArg("-o"),
-     bldmock.OutPath("tmp/test-output"), bldmock.InHash("4cb90a97dd7fc70b16eeefa5c3937769b7a35d7ba1a07db400b8d470dacbf030")},
+     bldmock.OutPath("tmp/test-output"), bldmock.InFile(fIn)},
     expected: &pb.MockCall{
     Binary: "example",
     Args: []*pb.Argument{
@@ -34,7 +43,7 @@ func Test() {
     {
     binary: "example",
     args: []interface{} { bldmock.StrArg("-t"), bldmock.StrArg("SOME STRING"), bldmock.StrArg("-o"),
-     bldmock.OutContent("I AM AN OUTPUT"), bldmock.InHash("4cb90a97dd7fc70b16eeefa5c3937769b7a35d7ba1a07db400b8d470dacbf030")},
+     bldmock.OutContent("I AM AN OUTPUT"), bldmock.InFile(fIn)},
     expected: &pb.MockCall{
     Binary: "example",
     Args: []*pb.Argument{
